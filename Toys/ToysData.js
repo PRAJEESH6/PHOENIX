0416ae13f2883f48dc9167d15d9de5e82525d138
PRAJEESH . S <136949903+PRAJEESH6@users.noreply.github.com>
2024-03-23 05:46:41 -0500

    Add files via upload

---
+++ Toys/ToysData.js
@@ -0,0 +1,355 @@
+
+    export const toys=[
+      {
+        "id": "toy001",
+        "name": "LEGO Classic Creative Bricks",
+        "price": 24.99,
+        "description": "Unleash your creativity with the versatile LEGO Classic Creative Bricks set.",
+        "image": "https://images6.alphacoders.com/443/thumb-1920-443554.jpg"
+      },
+      {
+        "id": "toy002",
+        "name": "Barbie Dreamhouse",
+        "price": 129.99,
+        "description": "Explore endless storytelling possibilities with the Barbie Dreamhouse playset.",
+        "image": "https://th.bing.com/th/id/OIP.dtgfoFVp7rxyFlo2pa-FDwHaEK?rs=1&pid=ImgDetMain"
+      },
+      {
+        "id": "toy003",
+        "name": "Hot Wheels Mega Garage",
+        "price": 39.95,
+        "description": "Race, store, and tune your Hot Wheels cars in the exciting Mega Garage playset.",
+        "image": "https://i5.walmartimages.com/asr/75469f2e-4a25-46e7-8139-877933577583_1.d590f70142fcf10e05a151958c69a768.jpeg"
+      },
+      {
+        "id": "toy004",
+        "name": "NERF Elite Disruptor Blaster",
+        "price": 12.99,
+        "description": "Experience fast-paced action with the NERF Elite Disruptor Blaster.",
+        "image": "https://th.bing.com/th/id/OIP.MZ5NYzST6wjob0ukbiUpkwHaHa?rs=1&pid=ImgDetMain"
+      },
+      {
+        "id": "toy005",
+        "name": "Fisher-Price Rock-a-Stack",
+        "price": 6.99,
+        "description": "Help develop motor skills with the classic Fisher-Price Rock-a-Stack toy.",
+        "image": "https://robotxworld.com/images/products/products/186858407-images-16970440131.jpg"
+      },
+      {
+        "id": "toy006",
+        "name": "Paw Patrol Ultimate Rescue Fire Truck",
+        "price": 59.99,
+        "description": "Join the Paw Patrol pups on rescue missions with the Ultimate Rescue Fire Truck playset.",
+        "image": "https://i5.walmartimages.com/asr/1ad9e34c-6c53-4516-8d27-a764de860368_2.150767066787a65899882a5eee7b890c.jpeg"
+      },
+      {
+        "id": "toy007",
+        "name": "LEGO Friends Mia's Tree House",
+        "price": 27.95,
+        "description": "Embark on adventures with Mia in the charming LEGO Friends Mia's Tree House set.",
+        "image": "https://image.made-in-china.com/2f0j00RmbTnGYIJocf/11671518-Remote-Car-Toys-for-Kids-12V-Rechargeable-Children-Toys-Car-Kids-Toys.jpg"
+      },
+      {
+        "id": "toy008",
+        "name": "Melissa & Doug Wooden Building Blocks Set",
+        "price": 19.99,
+        "description": "Promote imaginative play and creativity with the Melissa & Doug Wooden Building Blocks Set.",
+        "image": "https://i5.walmartimages.com/asr/4348ad38-9f3c-4429-8230-a07d4139893d_2.c6137853603cc188a4ab37f56a3ba4ac.jpeg"
+      },
+      {
+        "id": "toy009",
+        "name": "VTech Smart Shots Sports Center",
+        "price": 29.95,
+        "description": "Engage in active play and learn with the VTech Smart Shots Sports Center.",
+        "image": "https://i5.walmartimages.com/asr/e2e52e2a-e4ef-4250-a24c-6de43581edd0.84be883e103d54e07fd5dcfc256075a3.jpeg"
+      },
+      {
+        "id": "toy010",
+        "name": "Disney Princess Dress Up Trunk",
+        "price": 29.99,
+        "description": "Transform into your favorite Disney Princesses with the Dress Up Trunk playset.",
+        "image": "https://th.bing.com/th/id/OIP.krKbjmjB8Rbg2uN3-GWsEQHaHa?w=1024&h=1024&rs=1&pid=ImgDetMain"
+      },
+      {
+        "id": "toy011",
+        "name": "LEGO City Deep Space Rocket and Launch Control",
+        "price": 99.99,
+        "description": "Explore outer space with the exciting LEGO City Deep Space Rocket and Launch Control playset.",
+        "image": "https://th.bing.com/th/id/OIP.US6dmPVm_eKV8FNBw1dxXgHaHa?w=800&h=800&rs=1&pid=ImgDetMain"
+      },
+      {
+        "id": "toy012",
+        "name": "Crayola Inspiration Art Case",
+        "price": 19.95,
+        "description": "Fuel creativity with the comprehensive Crayola Inspiration Art Case.",
+        "image": "https://img.tttcdn.com/product/original/p/gu1/R/S/RM9334C-US/RM9334C-US-1-cd56-Kkph.jpg"
+      },
+      {
+        "id": "toy013",
+        "name": "VTech Go! Go! Smart Wheels Ultimate Corkscrew Tower",
+        "price": 49.99,
+        "description": "Experience twisting and turning fun with the VTech Go! Go! Smart Wheels Ultimate Corkscrew Tower playset.",
+        "image": "https://vootmart.com/images/thumbs/0001922_118-scale-remote-control-rock-crawler-4wd-off-road-monster-truck-remote-control-4x4-rc-car-water-pro.jpeg"
+      },
+      {
+        "id": "toy014",
+        "name": "Melissa & Doug Food Groups",
+        "price": 17.99,
+        "description": "Teach about healthy eating with the Melissa & Doug Food Groups playset.",
+        "image": "https://shop.makerfire.com/cdn/shop/files/MJXM163Brushlesstruck_700x700.jpg?v=1686733934"
+      },
+      {
+        "id": "toy015",
+        "name": "Fisher-Price Laugh & Learn Smart Stages Puppy",
+        "price": 16.99,
+        "description": "Introduce early learning concepts with the interactive Fisher-Price Laugh & Learn Smart Stages Puppy.",
+        "image": "https://i.pinimg.com/originals/bd/56/d3/bd56d3de052db3aeeb460bb9b568de80.jpg"
+      },
+      {
+        "id": "toy016",
+        "name": "LEGO DUPLO My First Number Train",
+        "price": 19.99,
+        "description": "Teach numbers and counting with the colorful LEGO DUPLO My First Number Train playset.",
+        "image": "https://th.bing.com/th/id/OIP.wApvN-xcPpjd5E8QAZu5HQHaHa?w=800&h=800&rs=1&pid=ImgDetMain"
+      },
+      {
+        "id": "toy017",
+        "name": "Hot Wheels Criss Cross Crash Track Set",
+        "price": 44.95,
+        "description": "Experience thrilling crashes and stunts with the Hot Wheels Criss Cross Crash Track Set.",
+        "image": "https://i.pinimg.com/originals/a6/9d/32/a69d3263aec33d9206683351fd3e4a47.jpg"
+      },
+      {
+        "id": "toy018",
+        "name": "NERF N-Strike Elite Rapidstrike CS-18 Blaster",
+        "price": 39.99,
+        "description": "Unleash rapid-fire action with the NERF N-Strike Elite Rapidstrike CS-18 Blaster.",
+        "image": "https://th.bing.com/th/id/OIP.1rAuq_KIlTe3b-XUiiraZQHaHa?w=800&h=800&rs=1&pid=ImgDetMain"
+      },
+      {
+        "id": "toy019",
+        "name": "VTech Touch and Learn Activity Desk Deluxe",
+        "price": 54.95,
+        "description": "Engage in interactive learning with the VTech Touch and Learn Activity Desk Deluxe.",
+        "image": "https://th.bing.com/th/id/OIP.3NLR67zehsB-Eter00HNpgHaHa?w=640&h=640&rs=1&pid=ImgDetMain"
+      },
+      {
+        "id": "toy020",
+        "name": "Barbie Dreamtopia Rainbow Cove Princess Castle",
+        "price": 39.95,
+        "description": "Enter the magical world of Dreamtopia with the Barbie Rainbow Cove Princess Castle playset.",
+        "image": "https://th.bing.com/th/id/OIP.PyI7yqnSrEZm7Ptz-XlKIAHaHa?w=800&h=800&rs=1&pid=ImgDetMain"
+      },
+      {
+        "id": "toy021",
+        "name": "LEGO Star Wars Millennium Falcon",
+        "price": 159.99,
+        "description": "Join the Star Wars adventure with the iconic LEGO Star Wars Millennium Falcon set.",
+        "image": "https://www.dhresource.com/0x0/f2/albu/g15/M00/9F/41/rBVa3V-VezSAMnD4AADAmaNHu0c312.jpg/wltoys-12428-rc-car-1-12-2-4g-4wd-remote.jpg"
+      },
+      {
+        "id": "toy022",
+        "name": "Play-Doh Kitchen Creations Magical Oven",
+        "price": 24.99,
+        "description": "Cook up imaginative Play-Doh dishes with the Kitchen Creations Magical Oven playset.",
+        "image": "https://st4.depositphotos.com/1033554/20315/i/450/depositphotos_203155954-stock-photo-rendering-toys-white-background.jpg"
+      },
+      {
+        "id": "toy023",
+        "name": "Melissa & Doug Deluxe Wooden Railway Train Set",
+        "price": 74.95,
+        "description": "Embark on railway adventures with the Melissa & Doug Deluxe Wooden Railway Train Set.",
+        "image": "https://i5.walmartimages.com/asr/81996559-e08e-4e92-8700-54ebe323f941_1.39fe17a565375d7bf5814e3317050555.jpeg"
+      },
+      {
+        "id": "toy024",
+        "name": "Paw Patrol Sea Patroller",
+        "price": 59.99,
+        "description": "Join the Paw Patrol on aquatic rescue missions with the Sea Patroller playset.",
+        "image": "https://i5.walmartimages.com/asr/09a20aac-04cb-4619-bbc4-3323ae08b014_1.bfc47a8943d6b3872f9c91e3058434de.jpeg"
+      },
+      {
+        "id": "toy025",
+        "name": "LEGO DUPLO Deluxe Box of Fun",
+        "price": 44.95,
+        "description": "Inspire creative play with the diverse LEGO DUPLO Deluxe Box of Fun set.",
+        "image": "https://th.bing.com/th/id/OIP.a5AeIH6xSeKiRPSDOsf_WwAAAA?rs=1&pid=ImgDetMain"
+      },
+      {
+        "id": "toy026",
+        "name": "Hatchimals CollEGGtibles",
+        "price": 9.95,
+        "description": "Hatch and collect adorable mini Hatchimals with the CollEGGtibles playset.",
+        "image": "https://th.bing.com/th/id/R.b6fdf6496d1d7d2ea393e1352764706b?rik=Nop%2bXeL4WmdsUw&riu=http%3a%2f%2fwww.3000toys.com%2fimages%2fMAISTO%2f15021-SET-A.jpg&ehk=2xjv0mLlcWA%2f0xcM4tYNeN60FwX10QfE15jXNRwb1ag%3d&risl=&pid=ImgRaw&r=0"
+      },
+      {
+        "id": "toy027",
+        "name": "Crayola Color Wonder Mess-Free Art Desk",
+        "price": 22.99,
+        "description": "Experience mess-free coloring and creativity with the Crayola Color Wonder Art Desk.",
+        "image": "https://i5.walmartimages.com/asr/75eb0fbe-066e-474f-9de7-a215281c03fb.e3780ab83f75e8347ca7758e87af6f08.jpeg"
+      },
+      {
+        "id": "toy028",
+        "name": "Fisher-Price Little People Caring for Animals Farm",
+        "price": 39.99,
+        "description": "Learn about animals and caring for them with the Little People Caring for Animals Farm playset.",
+        "image": "https://i5.walmartimages.com/asr/7a55689b-b543-4cd8-9ab4-bec6e1d332a3_1.2bb5e37dabc25a2a02166676c24bf85f.jpeg"
+      },
+      {
+        "id": "toy029",
+        "name": "VTech KidiZoom Smartwatch DX2",
+        "price": 49.95,
+        "description": "Stay connected and have fun with the VTech KidiZoom Smartwatch DX2.",
+        "image": "https://i5.walmartimages.com/asr/d190b5ea-5cb1-4fa3-9d97-53ca1de2158f.16a199d258642da3d5abb5a02dd91b1b.jpeg"
+      },
+      {
+        "id": "toy030",
+        "name": "LEGO Technic Bugatti Chiron",
+        "price": 349.99,
+        "description": "Build a detailed LEGO Technic replica of the Bugatti Chiron sports car.",
+        "image": "https://i5.walmartimages.com/asr/530ddb2f-c6a9-4839-932f-206a77165620.0da5bb3dd798b4af18db3b09f2ecb618.jpeg"
+      },
+      {
+        "id": "toy031",
+        "name": "NERF Fortnite AR-L Elite Dart Blaster",
+        "price": 34.99,
+        "description": "Bring Fortnite action to life with the NERF Fortnite AR-L Elite Dart Blaster.",
+        "image": "https://1.bp.blogspot.com/-0vUPKs8jJ0Y/WKLhR35GJ1I/AAAAAAAAA_k/BlGrsrx9iM0s_Y-orE-hvMP73IXcM1yTACLcB/s1600/81oEBdQjbrL._SL1500_.jpg"
+      },
+      {
+        "id": "toy032",
+        "name": "Melissa & Doug See & Spell Learning Toy",
+        "price": 19.99,
+        "description": "Practice spelling and word recognition with the Melissa & Doug See & Spell Learning Toy.",
+        "image": "https://th.bing.com/th/id/OIP.jwtBQZE9FO5NNhaA1Up5owHaHa?rs=1&pid=ImgDetMain"
+      },
+      {
+        "id": "toy033",
+        "name": "Hot Wheels Super Ultimate Garage",
+        "price": 159.95,
+        "description": "Race and play with the ultimate Hot Wheels Super Ultimate Garage playset.",
+        "image": "https://i5.walmartimages.com/asr/d8932afb-4ad4-4b00-89ea-7d280bb1169a.11de26f892853f9b5ab73de027afb908.jpeg"
+      },
+      {
+        "id": "toy034",
+        "name": "VTech Go! Go! Smart Wheels Mickey Magical Wonderland",
+        "price": 29.95,
+        "description": "Explore a magical world with Mickey Mouse and the VTech Go! Go! Smart Wheels Mickey Magical Wonderland playset.",
+        "image": "https://th.bing.com/th/id/R.c17efba391a877ab8e13d0877acdf3bc?rik=Qf7%2fT8mb80JELg&riu=http%3a%2f%2fwww.searchamateur.com%2fpictures%2fsecond-generation-robosapien-1.jpg&ehk=YD3Q0u4lMwkK8kuYeV9ZLKckhv7xupDHf08Gc%2fiaQCk%3d&risl=&pid=ImgRaw&r=0"
+      },
+      {
+        "id": "toy035",
+        "name": "Barbie Fashionistas Doll",
+        "price": 9.99,
+        "description": "Experience fashion and style with the diverse Barbie Fashionistas Doll collection.",
+        "image": "https://blog.brightminds.co.uk/wp-content/uploads/2019/12/Big-track-BrightMinds-Toys.jpeg"
+      },
+      {
+        "id": "toy036",
+        "name": "LEGO Harry Potter Hogwarts Castle",
+        "price": 399.99,
+        "description": "Recreate the magic of Hogwarts with the iconic LEGO Harry Potter Hogwarts Castle set.",
+        "image": "https://www.messforless.net/wp-content/uploads/2014/06/amazon13.jpg"
+      },
+      {
+        "id": "toy037",
+        "name": "Fisher-Price Think & Learn Rocktopus",
+        "price": 49.99,
+        "description": "Explore music and learning with the interactive Fisher-Price Think & Learn Rocktopus.",
+        "image": "https://i.pinimg.com/originals/aa/ba/40/aaba4066be6ed6e7ed3bb39097852c2e.jpg"
+      },
+      {
+        "id": "toy038",
+        "name": "Play-Doh Wheels Excavator and Loader",
+        "price": 16.95,
+        "description": "Create construction fun with the Play-Doh Wheels Excavator and Loader playset.",
+        "image": "https://th.bing.com/th/id/OIP.Un4VymQqVVIkHkdZlM6syAHaHa?rs=1&pid=ImgDetMain"
+      },
+      {
+        "id": "toy039",
+        "name": "VTech Write & Learn Creative Center",
+        "price": 21.99,
+        "description": "Practice writing and drawing with the VTech Write & Learn Creative Center.",
+        "image": "https://th.bing.com/th/id/OIP.grD--CeiC6NSMRuVFkT9LQHaHa?rs=1&pid=ImgDetMain"
+      },
+      {
+        "id": "toy040",
+        "name": "LEGO Technic Porsche 911 GT3 RS",
+        "price": 299.99,
+        "description": "Build an authentic LEGO Technic Porsche 911 GT3 RS model.",
+        "image": "https://i5.walmartimages.com/asr/2fe1dc82-ee46-4ecc-a4c1-afe2eac14fc8.55f8718060c910ace081414562a3ffa2.jpeg"
+      },
+      {
+        "id": "toy041",
+        "name": "Hatchimals Mystery",
+        "price": 12.95,
+        "description": "Hatch a surprise with the Hatchimals Mystery collectible toy.",
+        "image": "https://i5.walmartimages.com/asr/8a5b9454-a5c3-43f1-9685-a49486794f8d.32e8028d1a68f08020b4ce11593c9347.jpeg"
+      },
+      {
+        "id": "toy042",
+        "name": "Crayola Light-Up Tracing Pad",
+        "price": 24.95,
+        "description": "Explore tracing and drawing with the Crayola Light-Up Tracing Pad.",
+        "image": "https://images-na.ssl-images-amazon.com/images/I/814pBGeLNqL.jpg"
+      },
+      {
+        "id": "toy043",
+        "name": "Fisher-Price Imaginext DC Super Friends Batcave",
+        "price": 34.99,
+        "description": "Join Batman on exciting adventures with the Fisher-Price Imaginext DC Super Friends Batcave playset.",
+        "image": "https://i5.walmartimages.com/asr/b5f9866a-8bca-40b7-9a23-d30f47378a4a_1.b0f01312c94531a2b2566097e74b9498.jpeg?odnWidth=1000&odnHeight=1000&odnBg=ffffff"
+      },
+      {
+        "id": "toy044",
+        "name": "VTech Little Apps Tablet",
+        "price": 18.99,
+        "description": "Engage in interactive learning and play with the VTech Little Apps Tablet.",
+        "image": "https://cheaprccar.co.uk/image/cache/data/newprods/1759283_7-500x500.jpg"
+      },
+      {
+        "id": "toy045",
+        "name": "LEGO DUPLO Steam Train",
+        "price": 59.99,
+        "description": "Build and control the colorful LEGO DUPLO Steam Train set.",
+        "image": "https://th.bing.com/th/id/OIP.vCqfBB_daQ3MxSyfSJ9-nAHaHa?w=800&h=800&rs=1&pid=ImgDetMain"
+      },
+      {
+        "id": "toy046",
+        "name": "Baby Alive Real As Can Be Baby",
+        "price": 99.95,
+        "description": "Experience lifelike baby play with the interactive Baby Alive Real As Can Be Baby doll.",
+        "image": "https://th.bing.com/th/id/R.e310f1a85df0bea74b3231af12f99131?rik=mQOq1btrVPDk4g&riu=http%3a%2f%2fecx.images-amazon.com%2fimages%2fI%2f51v9S3zTMUL.jpg&ehk=bNMktyWC6qE3PVYCqKUvtwTZDH9kCrwBGThBmYYLl2I%3d&risl=&pid=ImgRaw&r=0"
+      },
+      {
+        "id": "toy047",
+        "name": "Melissa & Doug Deluxe Pounding Bench",
+        "price": 15.99,
+        "description": "Develop fine motor skills with the Melissa & Doug Deluxe Pounding Bench.",
+        "image": "https://images-na.ssl-images-amazon.com/images/I/81IrH5BEwqL._SL1500_.jpg"
+      },
+      {
+        "id": "toy048",
+        "name": "Hot Wheels Track Builder System",
+        "price": 29.95,
+        "description": "Create custom Hot Wheels tracks and stunts with the Track Builder System playset.",
+        "image": "https://th.bing.com/th/id/OIP.G1h8xWvfkmHq8qZMYxCpOgHaHa?rs=1&pid=ImgDetMain"
+      },
+      {
+        "id": "toy049",
+        "name": "Play-Doh Sparkle Compound Collection",
+        "price": 9.95,
+        "description": "Add shimmer and shine to your Play-Doh creations with the Sparkle Compound Collection.",
+        "image": "https://i.pinimg.com/originals/93/6c/62/936c62069d73a81f1ec96708df754a84.jpg"
+      },
+      {
+        "id": "toy050",
+        "name": "VTech Spin and Learn Color Flashlight",
+        "price": 11.95,
+        "description": "Explore colors and animals with the VTech Spin and Learn Color Flashlight.",
+        "image": "https://i.ebayimg.com/images/g/5KIAAOSwe6ZaYWUi/s-l640.png"
+      }
+    ]
+  
+  
