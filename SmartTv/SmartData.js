84652f780dae660bc28827bda73f03f7fb7ed3e1
PRAJEESH . S <136949903+PRAJEESH6@users.noreply.github.com>
2024-03-23 06:10:14 -0500

    Add files via upload

---
+++ SmartTv/SmartData.js
@@ -0,0 +1,215 @@
+
+    export const smartTVs = [
+      {
+        "id": "tv001",
+        "name": "Samsung 55-inch 4K Smart TV",
+        "price": 799.99,
+        "description": "Experience stunning visuals with this 55-inch 4K smart TV from Samsung.",
+        "image": "https://th.bing.com/th/id/OIP.G-Q68m9LlOWPAecP9NRoSwHaE8?pid=ImgDet&rs=1"
+      },
+      {
+        "id": "tv002",
+        "name": "LG OLED 65-inch 4K Smart TV",
+        "price": 1699.00,
+        "description": "Immerse yourself in the world of OLED technology with this 65-inch 4K smart TV from LG.",
+        "image": "https://i02.appmifile.com/mi-com-product/fly-birds/xiaomi-tv-a-pro-65/pc/49afa434ce167a23fb7fb9a37b9d745f.jpg"
+      },
+      {
+        "id": "tv003",
+        "name": "Sony Bravia 50-inch 4K Android TV",
+        "price": 999.95,
+        "description": "Enjoy entertainment like never before with this 50-inch 4K Android TV from Sony.",
+        "image": "https://thegadgetflow.com/wp-content/uploads/2021/02/Xiaomi-Mi-TV-Q1-75-Smart-Television-blog-featured-1024x576.jpeg"
+      },
+      {
+        "id": "tv004",
+        "name": "TCL Roku 55-inch Smart TV",
+        "price": 549.00,
+        "description": "Stream your favorite content effortlessly on this 55-inch smart TV with Roku.",
+        "image": "https://www.lg.com/us/images/signature/carousel/tv1.jpg"
+      },
+      {
+        "id": "tv005",
+        "name": "Vizio 75-inch Quantum 4K Smart TV",
+        "price": 1299.50,
+        "description": "Get ready for a vibrant viewing experience with this 75-inch Quantum 4K smart TV from Vizio.",
+        "image": "https://www.audioaffair.co.uk/blog/wp-content/uploads/2020/04/Optoma-UHZ65UST-Ultra-Short-Throw-4K-Laser-Projector.jpg"
+      },
+      {
+        "id": "tv006",
+        "name": "Hisense 43-inch Roku TV",
+        "price": 379.99,
+        "description": "Upgrade your home entertainment with this 43-inch Roku TV from Hisense.",
+        "image": "https://pisces.bbystatic.com/image2/BestBuy_US/images/products/6426/6426680cv17d.jpg"
+      },
+      {
+        "id": "tv007",
+        "name": "Panasonic 65-inch 4K HDR Smart TV",
+        "price": 1599.00,
+        "description": "Experience lifelike visuals and enhanced colors with this 65-inch 4K HDR smart TV from Panasonic.",
+        "image": "https://thegadgetflow.com/wp-content/uploads/2017/01/Sony-Short-Throw-4K-Projector-04.jpeg"
+      },
+      {
+        "id": "tv008",
+        "name": "Sharp Aquos 60-inch Smart TV",
+        "price": 799.95,
+        "description": "Enjoy smart features and a sleek design with this 60-inch Aquos smart TV from Sharp.",
+        "image": "https://cdn.shoplightspeed.com/shops/607339/files/1775135/sony-vpl-gtz14k-ultra-short-throw-laser-projector.jpg"
+      },
+      {
+        "id": "tv009",
+        "name": "RCA 32-inch HD Smart TV",
+        "price": 229.50,
+        "description": "Compact and affordable, this 32-inch HD smart TV from RCA is perfect for small spaces.",
+        "image": "https://www.hughes.co.uk/blog/wp-content/uploads/2018/10/blog-template-5-752x440.jpg"
+      },
+      {
+        "id": "tv010",
+        "name": "Insignia 50-inch Fire TV Edition",
+        "price": 499.99,
+        "description": "Experience seamless integration with Amazon Fire TV on this 50-inch Insignia smart TV.",
+        "image": "https://www.bigpicturebigsound.com/artman2/uploads/3/MartinLogan-Motion40-life.jpg"
+      },
+      {
+        "id": "tv011",
+        "name": "Philips 55-inch 4K Smart TV",
+        "price": 899.00,
+        "description": "Elevate your entertainment experience with this 55-inch 4K smart TV from Philips.",
+        "image": "https://th.bing.com/th/id/OIP.P4i5WLZ2vq_jPUJXWmRKBAHaEY?pid=ImgDet&rs=1"
+      },
+      {
+        "id": "tv012",
+        "name": "Sceptre 65-inch Android TV",
+        "price": 749.50,
+        "description": "Explore a world of apps and content on this 65-inch Android smart TV from Sceptre.",
+        "image": "https://i.pinimg.com/originals/20/30/8f/20308f33805391cbbe13df274f109084.jpg"
+      },
+      {
+        "id": "tv013",
+        "name": "Element 40-inch Full HD Smart TV",
+        "price": 299.00,
+        "description": "Enjoy a crisp and clear picture on this 40-inch full HD smart TV from Element.",
+        "image": "https://th.bing.com/th/id/OIP.s44zD6Su2nKNxfL2bUeTAQHaFj?pid=ImgDet&w=1920&h=1440&rs=1"
+      },
+      {
+        "id": "tv014",
+        "name": "Westinghouse 58-inch 4K Roku TV",
+        "price": 649.99,
+        "description": "Discover a world of entertainment options with this 58-inch 4K Roku TV from Westinghouse.",
+        "image": "https://i.pinimg.com/originals/9f/36/1c/9f361c0e257efff34e456f41d2e1c9e9.jpg"
+      },
+      {
+        "id": "tv015",
+        "name": "JVC 55-inch Roku TV",
+        "price": 449.95,
+        "description": "Stream your favorite shows and movies on this 55-inch Roku TV from JVC.",
+        "image": "https://th.bing.com/th/id/OIP.UHMK1U4caMAGwpu9xDsl0gHaE6?pid=ImgDet&w=736&h=489&rs=1"
+      },
+      {
+        "id": "tv016",
+        "name": "Sanyo 43-inch Smart TV",
+        "price": 329.50,
+        "description": "Experience a world of digital entertainment on this 43-inch smart TV from Sanyo.",
+        "image": "https://www.slashgear.com/img/gallery/vizio-unveils-holiday-lineup-of-hdtvs-and-sound-bars/intro-import.jpg"
+      },
+      {
+        "id": "tv017",
+        "name": "Haier 50-inch 4K Smart TV",
+        "price": 599.00,
+        "description": "Immerse yourself in 4K content with this 50-inch smart TV from Haier.",
+        "image": "https://th.bing.com/th/id/R.fce4c1f73afa558aeea6811e93db915f?rik=Mhb3DNt3OULH%2bQ&riu=http%3a%2f%2fwww.doityourself.com%2fforum%2fattachments%2fcarpentry-cabinetry-interior-woodworking%2f1081d1339425418-help-diy-floating-entertainment-unit-1.jpg&ehk=B8esWLyT%2bBpfm5vOk3woJghRZ8H%2ffh%2bNj16WqfvNm0s%3d&risl=&pid=ImgRaw&r=0"
+      },
+      {
+        "id": "tv018",
+        "name": "Toshiba 55-inch Fire TV Edition",
+        "price": 599.99,
+        "description": "Enjoy seamless streaming and voice control with this 55-inch Fire TV Edition from Toshiba.",
+        "image": "https://th.bing.com/th/id/R.823dbc7f46c26f560de99d4680f5b340?rik=hnY%2bNbm%2fzmMU2A&riu=http%3a%2f%2fwww.flatpanelshd.com%2fpictures%2fphilipscinema2193d-5l.jpg&ehk=ET1t1WrzhWy215QTFeGMN%2bGa7vKWuve4TzJ0L%2fcyLHs%3d&risl=&pid=ImgRaw&r=0"
+      },
+      {
+        "id": "tv019",
+        "name": "Roku Premiere 4K Streaming Stick",
+        "price": 69.99,
+        "description": "Turn any TV into a smart TV with the Roku Premiere 4K streaming stick.",
+        "image": "https://th.bing.com/th/id/R.6bbac8de0004cbafc0fc8fe96eea2641?rik=IpKlgkDYcphY7Q&riu=http%3a%2f%2fwww.techvicity.com%2fwp-content%2fuploads%2f2016%2f08%2fPhilips-4k-LED-TV.jpg&ehk=vYW%2fd%2fz75gWj0H5b40y2ZGeZvw%2fAD93kvLpCQ4SF3vs%3d&risl=&pid=ImgRaw&r=0"
+      },
+      {
+        "id": "tv020",
+        "name": "LG NanoCell 75-inch 8K Smart TV",
+        "price": 2999.00,
+        "description": "Experience stunning 8K visuals on this 75-inch NanoCell smart TV from LG.",
+        "image": "https://i0.wp.com/avantela.com/home/wp-content/uploads/2019/04/TV-Above-DVD-Storage-Idea.jpg?fit=924%2C611&ssl=1&is-pending-load=1"
+      },
+      {
+        "id": "tv021",
+        "name": "Sony XBR 85-inch 8K Smart TV",
+        "price": 4999.95,
+        "description": "Enter the world of 8K resolution with this 85-inch smart TV from Sony.",
+        "image": "https://th.bing.com/th/id/R.3da3d8c0769ef662227c2765094c5346?rik=tX0l7hNCfzqpWA&riu=http%3a%2f%2fi2.au.reastatic.net%2fhome-ideas%2f476x357%2f1e8702bc7b910f0c188e01da6eef99f9ccc25dd4dab49d6749aa7aba1fa98ab4%2fliving%2bareas.jpg&ehk=pSAjsGUva7yFJ4aVXBmaBlQaJ1mCvtaZpdqbnRvUnRA%3d&risl=&pid=ImgRaw&r=0"
+      },
+      {
+        "id": "tv022",
+        "name": "Vizio M-Series 60-inch Quantum 4K TV",
+        "price": 999.50,
+        "description": "Upgrade your home theater with the vibrant colors of this 60-inch Quantum 4K TV from Vizio.",
+        "image": "https://th.bing.com/th/id/R.3bb86dc030b2d243bfc26f706a6b5ecf?rik=ffTmTVJw3kqc4w&riu=http%3a%2f%2fflatpanelshd.com%2fpictures%2fsonylife2011-3l.jpg&ehk=3p3uitPGIUT0NgyEV6jhzxNPzwzw9cvh0wszdRgQ%2btg%3d&risl=&pid=ImgRaw&r=0"
+      },
+      {
+        "id": "tv023",
+        "name": "Insignia 32-inch Fire TV Edition",
+        "price": 249.99,
+        "description": "Enjoy easy access to streaming services on this 32-inch Fire TV Edition from Insignia.",
+        "image": "https://www.awesome11.com/wp-content/uploads/2016/07/Contemporary-Living-Room-With-Modern-Touch-Interior-768x569.jpg"
+      },
+      {
+        "id": "tv024",
+        "name": "Philips 65-inch Ambilight Smart TV",
+        "price": 1299.00,
+        "description": "Enhance your viewing experience with immersive Ambilight technology on this 65-inch smart TV from Philips.",
+        "image": "https://cdn11.bigcommerce.com/s-5si61j/images/stencil/960w/products/265/3079/final_4__19839.1603018395.jpg?c=2"
+      },
+      {
+        "id": "tv025",
+        "name": "Sharp 70-inch 4K Android TV",
+        "price": 1499.95,
+        "description": "Upgrade your home entertainment with this 70-inch 4K Android TV from Sharp.",
+        "image": "https://i.pinimg.com/originals/c1/9f/08/c19f081589d9b39b097afbb142c3e3e0.jpg"
+      },
+      {
+        "id": "tv026",
+        "name": "RCA 55-inch QLED Roku TV",
+        "price": 799.99,
+        "description": "Experience brilliant colors and lifelike visuals on this 55-inch QLED Roku TV from RCA.",
+        "image": "https://th.bing.com/th/id/R.55f1fb2bc2902100b0ebcde9f16db190?rik=UupOUv1PzopHfw&riu=http%3a%2f%2fcdn.home-designing.com%2fwp-content%2fuploads%2f2014%2f02%2fLiving-Room-Bookshelves-31.jpg&ehk=YIkb9DMp0%2fOhMmkzRz1d%2f933p5e3uzXxmWiL%2fnmZqDM%3d&risl=&pid=ImgRaw&r=0"
+      },
+      {
+        "id": "tv027",
+        "name": "TCL 75-inch 8-Series Roku TV",
+        "price": 1999.00,
+        "description": "Immerse yourself in stunning 8K content with this 75-inch 8-Series Roku TV from TCL.",
+        "image": "https://th.bing.com/th/id/R.197954398f9707dcd35b6272b81d86b2?rik=w4cN2nPaDxrwTQ&riu=http%3a%2f%2fst.houzz.com%2fsimgs%2f985135fd00382a2b_4-8098%2fmodern-basement.jpg&ehk=O4pFMDwIsZKsQt7on9DPllx1drJJdrSe9qjyaTB0vFc%3d&risl=&pid=ImgRaw&r=0"
+      },
+      {
+        "id": "tv028",
+        "name": "Sceptre 32-inch HD Smart TV",
+        "price": 249.50,
+        "description": "Experience HD clarity and smart features on this 32-inch smart TV from Sceptre.",
+        "image": "https://i.pinimg.com/originals/da/3e/b8/da3eb87f89a4dd4d64d7ba5f62e9cfeb.jpg"
+      },
+      {
+        "id": "tv029",
+        "name": "Element 65-inch 4K Smart TV",
+        "price": 799.00,
+        "description": "Upgrade your entertainment setup with this 65-inch 4K smart TV from Element.",
+        "image": "https://i.pinimg.com/originals/78/8c/1e/788c1e7c0f4c9723435652ab837328eb.jpg"
+      },
+      {
+        "id": "tv030",
+        "name": "Hisense 58-inch Android TV",
+        "price": 649.95,
+        "description": "Explore a world of apps and entertainment on this 58-inch Android TV from Hisense.",
+        "image": "https://2.bp.blogspot.com/-FHFupJBPKvo/WjpIf2u2XVI/AAAAAAAACCY/CPZ_K7OOxGw_8KZQKPX-Kn4CHUxhynkoQCEwYBhgL/s1600/neoclassical-interior-design-neoclassic-style%2B%25284%2529.jpg"
+      }
+    ]
+  
+  
